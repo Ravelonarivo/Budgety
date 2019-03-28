3a8c945d2fd643780283f85f581deeaba4ad3483
Irina <irinaravelonarivo@gmail.com>
2019-03-28 20:26:36 +0100

    dom elements

---
+++ src/js/views/base.js
@@ -0,0 +1,6 @@
+export const elements = {
+    addType: document.querySelector('.add__type'),
+    addDescription: document.querySelector('.add_description'),
+    addValue: document.querySelelector('.add_value'),
+    addButton: document.querySelector('.add_btn')
+};
